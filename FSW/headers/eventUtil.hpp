3e115a17142d9e241f58438adcdf3768c2de7848
Vysares <71406782+Vysares@users.noreply.github.com>
2021-02-02 10:51:50 -0700

    Event Sytem Initial commit
    Used for inter-module communication and timing

---
+++ FSW/headers/eventUtil.hpp
@@ -0,0 +1,52 @@
+#ifndef EVENTUTIL_H
+#define EVENTUTIL_H 
+
+/* - - - - - - Includes - - - - - - */
+// C++ libraries
+#include <vector>
+
+// Other libraries
+#include <Arduino.h>
+
+// NS2 headers
+
+
+/* - - - - - - Declarations - - - - - - */
+
+/* - - - - - - Class Definitions - - - - - - */
+
+class Event
+{
+    protected:
+        bool isInvoked;
+        
+    public:
+        Event();
+        bool checkInvoked();
+        void invoke();
+};
+
+
+class TimedEvent : public Event
+{
+    protected:
+        unsigned long duration;
+        unsigned long currentMillis;
+        unsigned long nextInvokeMillis;
+
+    public:
+        TimedEvent(unsigned long newDuration);
+        void setDuration(unsigned long newDuration);
+        bool checkInvoked();
+        void start();
+};
+
+class RecurringEvent : public TimedEvent
+{
+    public:
+        RecurringEvent(unsigned long newDuration);
+        bool checkInvoked();
+};
+
+
+#endif
