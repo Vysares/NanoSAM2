e4cb2992d42cef3140f200d0e4b179be330cb9a0
jaretanderson <jaretanderson2@gmail.com>
2021-02-02 22:38:45 -0700

    updated constants in configuration header

--- FSW/src/headers/config.hpp
+++ FSW/src/headers/config.hpp
@@ -1,16 +1,50 @@
 #ifndef CONFIG_H
 #define CONFIG_H 
-/* config.hpp is a header file 
+/* config.hpp contains mission-wide constants
  * Usage:
  *  define any mission-wide constants (pin numbers, 
  *  sampling rates, etc) that we will want to be able to change
  *  from a single location and include in multiple modules
  */
 
-/* - - - - - - Declarations - - - - - - */
-float dataProcessing();
-void scienceMemoryHandling();
-void updateBuffer(float sample, int &index);
-bool saveBuffer(int &index);
+
+/* 
+ * = = = = = = Configuration Declarations  = = = = = = 
+ */
+
+/* - - - - - - Teensy Pins - - - - - - */
+const int PIN_WD_RESET = 2; // Watchdog reset pin
+const int PIN_ACD_CS = 10;  // ADC Chip Select Pin
+
+/* - - - - - - SPI - - - - - - */
+const int SPI_MAX_SPEED = 2000000; // clock speed of 2MHz for SPI
+
+/* - - - - - - Serial - - - - - - */
+const int SERIAL_BAUD = 9600;
+const int SERIAL_TIMEOUT = 50;      // time to wait for serial input, ms
+
+/* 
+ * = = = = = = Module Constants  = = = = = = 
+ */
+
+/* - - - - - - Memory Handling Module - - - - - - */
+const int SAMPLING_RATE = 50;   // Hz, desired irradiance sampling rate
+const int WINDOW_LENGTH = 240;  // seconds, length of science data buffer 
+const int MAXFILES = 10;        // maximum number of files in flash storage
+const float ADC_BINS = 65536;       // bins, number of bins in ADC (2^16)
+const float ADC_MAX_VOLTAGE = 3.3;  // Volts, upper end of ADC voltage range
+const float ADC_MIN_VOLTAGE = 0.0;  // Volts, lower end of ADC voltage range
+
+// TODO: Update this with size of actual timestamp once it is known
+const int TIMESTAMP_SIZE = 1;   // array indices needed to store timestamp
+
+// set number of measurements to store in science data buffer
+const int BUFFERSIZE = SAMPLING_RATE * WINDOW_LENGTH; // indices
+const int FILESIZE = BUFFERSIZE + TIMESTAMP_SIZE;
+
+
+/* - - - - - - Fault Mitigation Module - - - - - - */
+const int WD_RESET_INTERVAL = 100;  // watchdog feeding interval, ms
+const int WD_PULSE_DUR = 10;        // watchdog reset signal duration, MICROSECONDS!!!
 
 #endif
