c5e1e4f84919c597bf8dd2cb4fb108fcf32c3023
Vysares <71406782+Vysares@users.noreply.github.com>
2021-02-08 21:22:04 -0700

    update hkUtil hpp & cpp

---
+++ FSW/src/headers/hkUtil.hpp
@@ -0,0 +1,15 @@
+#ifndef HKUTIL_H
+#define HKUTIL_H 
+
+/* - - - - - - Includes - - - - - - */
+// C++ libraries
+
+// Other libraries
+
+// NS2 headers
+#include "config.hpp"
+
+/* - - - - - - Declarations - - - - - - */
+void handleHousekeeping();
+
+#endif
---
+++ FSW/src/util/hkUtil.cpp
@@ -0,0 +1,67 @@
+/* hkUtil.cpp handles NS2 payload housekeeping
+ * Usage:
+ *  module functionality
+ *      system state data collection
+ *      temperature control
+ * 
+ * Modules encompassed:
+ *  houskeeping
+ *  thermal control
+ *
+ * Additional files needed for compilation:
+ *  none
+ */
+
+/* - - - - - - Includes - - - - - - */
+// C++ libraries
+
+// Other libraries
+
+// NS2 headers
+#include "../headers/hkUtil.hpp"
+
+
+// safe temperature range
+const int MIN_SAFE_TEMP = 50;
+const int MAX_SAFE_TEMP = 0;
+
+// pins
+const int DIGITAL_THERM_PIN = 14;
+const int ANALOG_THERM_PIN = 15;
+const int OPTICS_THEM_PIN = 16;
+
+/* Module Variable Definitions */
+
+
+
+/* - - - - - - Module Driver Functions - - - - - - */
+
+/* - - - - - - handleHousekeeping - - - - - - *
+ * Usage:
+ *  Reads incoming data from ADC, measures against baseline,
+ *  appends time and pointing data, sends to memory.
+ * 
+ * Inputs:
+ *  none
+ *  
+ * Outputs:
+ *  None
+ */
+void handleHousekeeping()
+{
+
+}
+
+/* - - - - - - Module Helper Functions - - - - - - */
+
+/* - - - - - - handleHousekeeping - - - - - - *
+ * Usage:
+ *  Reads incoming data from ADC, measures against baseline,
+ *  appends time and pointing data, sends to memory.
+ * 
+ * Inputs:
+ *  none
+ *  
+ * Outputs:
+ *  None
+ */
