1061a5c85856752386f50cd3303ae2a0a685f9f4
Vysares <71406782+Vysares@users.noreply.github.com>
2021-02-14 15:25:29 -0700

    housekeeping util functionality
    Housekeeping module initial functionality complete

--- FSW/src/headers/hkUtil.hpp
+++ FSW/src/headers/hkUtil.hpp
@@ -10,17 +10,18 @@
 #include "config.hpp"
 
 
-
 /* - - - - - - Declarations - - - - - - */
 void handleHousekeeping();
 void setHeater();
 void sampleHousekeepingData();
+float voltageToTemp(float voltage);
+void timeSortHkData();
 
 struct HkData // stores housekeeping data
 {
-    float tempOptics;      // optics bench temp, celsius
-    float tempAnalog;      // analog board temp, celsius
-    float tempDigital;     // digital board temp, celsius
+    float opticsTemp;      // optics bench temp, celsius
+    float analogTemp;      // analog board temp, celsius
+    float digitalTemp;     // digital board temp, celsius
     float analogCurrent;   // analog board current, amps
     float digitalCurrent;  // digital board current, amps
     float digitalRegPG;    // digital regulator "power good" signal, volts
@@ -28,46 +29,21 @@ struct HkData // stores housekeeping data
 
 struct TempVoltagePair { float temperature, voltage; };
 
-const TempVoltagePair thermLookup[] = { { -40.000F, 2.999F },
-                                        { -38.000F, 2.957F },
-                                        { -36.000F, 2.911F },
-                                        { -34.000F, 2.861F },
-                                        { -32.000F, 2.806F },
-                                        { -30.000F, 2.747F },
-                                        { -28.000F, 2.683F },
-                                        { -26.000F, 2.616F },
-                                        { -24.000F, 2.544F },
-                                        { -22.000F, 2.468F },
+// thermistor temperature/voltage lookup table
+const TempVoltagePair thermLookup[] = { { -30.000F, 2.747F },
+                                        { -25.000F, 2.580F },
                                         { -20.000F, 2.388F },
-                                        { -18.000F, 2.305F },
-                                        { -16.000F, 2.220F },
-                                        { -14.000F, 2.133F },
-                                        { -12.000F, 2.043F },
+                                        { -15.000F, 2.177F },
                                         { -10.000F, 1.953F },
-                                        { -8.000F, 1.862F },
-                                        { -6.000F, 1.771F },
-                                        { -4.000F, 1.681F },
-                                        { -2.000F, 1.592F },
+                                        { -5.000F, 1.726F },
                                         { 0.000F, 1.505F },
-                                        { 2.000F, 1.419F },
-                                        { 4.000F, 1.336F },
-                                        { 6.000F, 1.256F },
-                                        { 8.000F, 1.179F },
+                                        { 5.000F, 1.296F },
                                         { 10.000F, 1.105F },
-                                        { 12.000F, 1.035F },
-                                        { 14.000F, 0.967F },
-                                        { 16.000F, 0.904F },
-                                        { 18.000F, 0.844F },
+                                        { 15.000F, 0.935F },
                                         { 20.000F, 0.787F },
-                                        { 22.000F, 0.734F },
-                                        { 24.000F, 0.684F },
-                                        { 26.000F, 0.637F },
-                                        { 28.000F, 0.593F },
+                                        { 25.000F, 0.660F },
                                         { 30.000F, 0.552F },
-                                        { 32.000F, 0.514F },
-                                        { 34.000F, 0.479F },
-                                        { 36.000F, 0.446F },
-                                        { 38.000F, 0.415F },
+                                        { 35.000F, 0.462F },
                                         { 40.000F, 0.387F } };
 
 
--- FSW/src/util/hkUtil.cpp
+++ FSW/src/util/hkUtil.cpp
@@ -21,7 +21,6 @@
 #include "../headers/hkUtil.hpp"
 
 
-
 /* Module Variable Definitions */
 static HkData hkData[HK_SAMPLES_TO_KEEP] = {}; // array to store housekeeping data
 static int hkIndex = 0;
@@ -31,8 +30,9 @@ static int hkIndex = 0;
 
 /* - - - - - - handleHousekeeping - - - - - - *
  * Usage:
- *  Reads incoming data from ADC, measures against baseline,
- *  appends time and pointing data, sends to memory.
+ *  Handles all housekeeping functions.
+ *  Samples temperature and power data, handles thermal control
+ *  
  * 
  * Inputs:
  *  none
@@ -42,15 +42,19 @@ static int hkIndex = 0;
  */
 void handleHousekeeping()
 {
-
+    sampleHousekeepingData();
+    setHeater();
+    hkIndex++;
+    if (hkIndex >= HK_SAMPLES_TO_KEEP) { hkIndex = 0; }
 }
 
 /* - - - - - - Module Helper Functions - - - - - - */
 
-/* - - - - - - handleHousekeeping - - - - - - *
+/* - - - - - - setHeater - - - - - - *
  * Usage:
- *  Reads incoming data from ADC, measures against baseline,
- *  appends time and pointing data, sends to memory.
+ *  Toggles heater based on current optics bench temperature
+ *  Turns heater on when optics temp crosses below HEATER_TEMP_LOW
+ *  Turns heater off when optics temp crosses above HEATER_TEMP_HIGH
  * 
  * Inputs:
  *  none
@@ -60,18 +64,117 @@ void handleHousekeeping()
  */
 void setHeater()
 {
-
+    if (hkData[hkIndex].opticsTemp >= HEATER_TEMP_HIGH)
+    {
+        digitalWrite(PIN_HEAT, LOW); // turn heater off
+    }
+    else if (hkData[hkIndex].opticsTemp <= HEATER_TEMP_LOW)
+    {
+        digitalWrite(PIN_HEAT, HIGH); // turn heater on
+    }
 }
 
+/* - - - - - - sampleHousekeepingData - - - - - - *
+ * Usage:
+ *  samples temperature and power data and stores it in hkData,
+ *  checks for out-of-bounds values and logs faults if found
+ * 
+ * Inputs:
+ *  none
+ *  
+ * Outputs:
+ *  None
+ */
 void sampleHousekeepingData()
 {
-    hkData[hkIndex].tempAnalog = analogRead(PIN_ANALOG_THERM);
-    hkData[hkIndex].tempDigital = analogRead(PIN_DIGITAL_THERM);
-    hkData[hkIndex].tempOptics = analogRead(PIN_OPTICS_THERM);
-    hkData[hkIndex].analogCurrent = VOLTAGE_RES*(float)analogRead(PIN_AREG_CURR);   // analog regulator current, Amps
-    hkData[hkIndex].digitalCurrent = VOLTAGE_RES*(float)analogRead(PIN_DREG_CURR);   // digital regulator current, Amps
-    hkData[hkIndex].digitalRegPG = VOLTAGE_RES*(float)analogRead(PIN_DREG_PG);    // digital regulator voltage 
-    hkIndex++;
+    // read thermistor voltage values
+    float opticsTempVoltage = TEENSY_VOLTAGE_RES*analogRead(PIN_OPTICS_THERM);
+    float analogTempVoltage = TEENSY_VOLTAGE_RES*analogRead(PIN_ANALOG_THERM);
+    float digitalTempVoltage = TEENSY_VOLTAGE_RES*analogRead(PIN_DIGITAL_THERM);
+    
+    // update housekeeping data
+    hkData[hkIndex].opticsTemp = voltageToTemp(opticsTempVoltage); 
+    hkData[hkIndex].analogTemp = voltageToTemp(analogTempVoltage);
+    hkData[hkIndex].digitalTemp = voltageToTemp(digitalTempVoltage);
+    hkData[hkIndex].analogCurrent = TEENSY_VOLTAGE_RES*(float)analogRead(PIN_AREG_CURR);
+    hkData[hkIndex].digitalCurrent = TEENSY_VOLTAGE_RES*(float)analogRead(PIN_DREG_CURR);   
+    hkData[hkIndex].digitalRegPG = TEENSY_VOLTAGE_RES*(float)analogRead(PIN_DREG_PG);
+
+    // Check if values are in acceptable ranges
+    // TODO: Log a fault for every case
+    if (hkData[hkIndex].opticsTemp > OPTICS_TEMP_MAX_SAFE) { } // optics temp too high
+    else if (hkData[hkIndex].opticsTemp < OPTICS_TEMP_MIN_SAFE) { } // optics temp too low
+
+    if (hkData[hkIndex].analogTemp > BOARD_TEMP_MAX_SAFE) { } // analog board temp too high
+    else if (hkData[hkIndex].analogTemp < BOARD_TEMP_MIN_SAFE) { } // analog board temp too low
+
+    if (hkData[hkIndex].digitalTemp > BOARD_TEMP_MAX_SAFE) { } // digital board temp too high
+    else if (hkData[hkIndex].digitalTemp < BOARD_TEMP_MIN_SAFE) { } // digital board temp too low
+
+    if (hkData[hkIndex].digitalRegPG > PG_VOLTAGE_MAX_EXPECTED) { } // digital regulator PG signal too high
+    else if (hkData[hkIndex].digitalRegPG < PG_VOLTAGE_MIN_EXPECTED) { } // digital regulator PG signal too low
+
 }
 
+/* - - - - - - voltageToTemp - - - - - - *
+ * Usage:
+ *  Interpolates and returns the corresponding temperature for a given thermistor voltage reading
+ *  based on the data in thermLookup. 
+ *  Will extrapolate beyond max and min entries in thermLookup if needed
+ * 
+ * Inputs:
+ *  voltage - thermistor voltage
+ *  
+ * Outputs:
+ *  temperature - celsius, the temperature corresponding to thermistor voltage reading
+ */
+float voltageToTemp(float voltage)
+{
+    int thermLookupSize = sizeof(thermLookup) / sizeof(thermLookup[0]); // size of lookup table
+    
+    // find lower adjacent entry
+    int i = 0;
+    while (thermLookup[i].voltage < voltage && i < thermLookupSize - 1) { i++; }
+    
+    float tempGap = thermLookup[i + 1].temperature - thermLookup[i].temperature; // temp difference between upper and lower entries
+    float voltageGap = thermLookup[i + 1].voltage - thermLookup[i].voltage; // voltage difference between upper and lower entries
+    
+    // interpolate temperature
+    float temperature = tempGap*(voltage - thermLookup[i].voltage) / voltageGap;
+    return temperature;
+}
 
+/* - - - - - - timeSortHkData - - - - - - *
+ * Usage:
+ *  Sorts hkData in ascending cronological order and resets hkIndex to 0
+ *  call before using hkData in another function
+ * 
+ * Inputs:
+ *  none
+ *  
+ * Outputs:
+ *  none
+ */
+void timeSortHkData()
+{
+    // create array to hold data in time ascending order
+    HkData timeSortHkData[HK_SAMPLES_TO_KEEP];
+    int j = 0; // iterator for sorted array index
+
+    // reorder array so that it is ascending in time
+    for (int i = hkIndex; i < HK_SAMPLES_TO_KEEP; i++)
+    {
+        timeSortHkData[j] = hkData[i];
+        j++;
+    } 
+
+    // loop back to top of array and store remaining values
+    for (int i = 0; i < hkIndex; i++)
+    {
+        timeSortHkData[j] = hkData[i];
+        j++;
+    }
+
+    hkIndex = 0;
+    hkData = timeSortHkData;
+}
