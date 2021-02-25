9e365ad8b9b823811c712c51d17f3d6aa74b62e0
Vysares <71406782+Vysares@users.noreply.github.com>
2021-02-24 21:15:37 -0700

    Reformated edac header file

--- FSW/src/headers/edac.hpp
+++ FSW/src/headers/edac.hpp
@@ -11,23 +11,53 @@
 #include "config.hpp"
 
 /* - - - - - - Bit Operation Macros - - - - - - */
-// a=target variable, bit=bit number to act upon 0-n */
-#define BIT_SET(a,bit) ((a) |= (1ULL<<(bit)))
-#define BIT_CLEAR(a,bit) ((a) &= ~(1ULL<<(bit)))
-#define BIT_FLIP(a,bit) ((a) ^= (1ULL<<(bit)))
-#define BIT_CHECK(a,bit) (!!((a) & (1ULL<<(bit)))) // '!!' to make sure this returns 0 or 1
+// a = target variable, bit = bit number to act on (0 indexed)
+#define BIT_SET(a,bit) ((a) |= (1ULL<<(bit)))       // sets bit to 1
+#define BIT_CLEAR(a,bit) ((a) &= ~(1ULL<<(bit)))    // clears bit to 0
+#define BIT_FLIP(a,bit) ((a) ^= (1ULL<<(bit)))      // flips bit
+#define BIT_CHECK(a,bit) (!!((a) & (1ULL<<(bit))))  // returns bit value, 0 or 1
 
-struct ErrorReport {
-    bool errorDetected = false;
-    int errorSize = 0;
-    int errorPosition = -1;
+
+/* - - - - - - Structs - - - - - - */
+
+/* - ErrorReport -
+*   Holds error info for a single hamming block.
+*   Members: size, position
+*/
+struct ErrorReport { 
+    int size = 0;      // size of detected error, 0 for no detected error
+    int position = -1; // position of error if errorSize is 1, -1 otherwise
 };
 
+/* - ScrubReport -
+*   Holds results of file scrub.
+*   Members: numErrors, corrected, uncorrected 
+*/
+struct ScrubReport { 
+    int numErrors = 0;      // number of errors detected
+    int corrected = 0;      // number of single bit errors corrected
+    int uncorrected = 0;    // number of errors detected but not corrected
+};
+
+/* - DecodedFile -
+*   Holds readable science data and scrub report from decoded file.
+*   Members: photdiode16, timestamp, scrubInfo 
+*/
+struct DecodedFile { 
+    uint16_t photdiode16[BUFFERSIZE] = {};  // array of photodiode ADC bin numbers
+    unsigned long timestamp = 0UL;          // timestamp
+    ScrubReport scrubInfo;                  // contains info on errors
+};
+
+
 /* - - - - - - Declarations - - - - - - */
-void encodeScienceData(uint8_t *destination, uint16_t *buffer, unsigned long &timestamp);
+void encodeFile(uint8_t *destination, uint16_t *buffer, unsigned long &timestamp);
+DecodedFile decodeFile(uint8_t *encodedData);
 void encodeHamming_72_64(uint8_t *hammingBlock, uint8_t *messageBlock);
 void decodeHamming_72_64(uint8_t *hammingBlock, uint8_t *messageBlock);
-ErrorReport scan(uint8_t *hammingBlock);
+ErrorReport scanBlock(uint8_t *hammingBlock);
+ErrorReport correctBlock(uint8_t *hammingBlock);
+ScrubReport scrubFile(uint8_t *encodedData);
 void printBlock(uint8_t *hammingBlock);
 bool checkBit(uint8_t *byteArray, int index);
 void assignBit(uint8_t *byteArray, int index, bool val);
