280974d4ce76bf1b7a9f2b12db3eaef8ca636fd4
jaretanderson <jaretanderson2@gmail.com>
2021-02-25 17:28:13 -0700

    first attempt at unit testing

---
+++ UnitTest/TestScripts/timingTest.cpp
@@ -0,0 +1,79 @@
+/* unitTestDriver.cpp is the main script for the NanoSAM II Unit Testing 
+ * Usage:
+ *  part of NS2 unit testing suite
+ *  to be called in unitTestDriver.cpp
+ *
+ */
+
+
+// NS2 includes
+#include "../headers/config.hpp"
+//#include "../headers/timingClass.hpp"
+#include "../headers/timing.hpp"
+
+/* - - - - - - testWrapBufferIdx - - - - - - *
+ * Usage:
+ * tests wrapBufferIdx with variety of idx
+ * 
+ * Inputs:
+ *  none
+ *  
+ * Outputs:
+ *  0 - pass
+ *  1 - fail
+ */
+int testWrapBufferIdx() {
+
+    // call function to be tested
+
+    int key[] = {0, BUFFERSIZE-1, BUFFERSIZE-1, 0, 1, 0};
+    int testVal[] = {0, -1, BUFFERSIZE-1, BUFFERSIZE, 2*BUFFERSIZE + 1, -BUFFERSIZE};
+
+    for (int i=0, i++, i<6) {
+        if (key[i] != wrapBufferIdx(testVal[i])) {
+            Serial.println("wrapBufferIdx() Unit Test Failed (timing module)")
+            return 1;
+        }
+    }
+
+    return 0;
+}
+
+
+/* - - - - - - testVoltageRunningMean - - - - - - *
+ * Usage:
+ * tests voltageRunningMean with variety of buffers
+ * 
+ * Inputs:
+ *  none
+ *  
+ * Outputs:
+ *  0 - pass
+ *  1 - fail
+ */
+int testVoltageRunningMean() {
+
+    // call function to be tested
+
+    return 1;
+}
+
+
+/* - - - - - - timingTestMain - - - - - - *
+ * Usage:
+ * runs the timing module unit tests, prints results over serial
+ *  must be kept last in file since we are not using header structure for testing
+ * 
+ * Inputs:
+ *  none
+ *  
+ * Outputs:
+ *  number of tests that failed in module
+ */
+int timingTestMain(){
+    int testsFailed = 0; // iterator to track how many tests have failed
+
+    testsFailed += testWrapBufferIdx();
+
+    return testsFailed;
+}
---
+++ UnitTest/unitTestDriver.cpp
@@ -0,0 +1,37 @@
+/* unitTestDriver.cpp is the main script for the NanoSAM II Unit Testing 
+ * Usage:
+ *  run this script through Teensyduino to execute NS2 unit testing suite
+ *  see instructions.md in this directory for detailed instructions on compilation
+ *
+ */
+
+
+/* - - - - - - main - - - - - - *
+ * Usage:
+ * call each module unit test here
+ *  comment/uncomment if you want to enable/disable specific tests
+ * 
+ * Inputs:
+ *  none
+ *  
+ * Outputs:
+ *  None
+ */
+int main(){
+
+    int testFailCount = 0; // track how many total tests failed
+
+    // testing functions 
+    testFailCount += timingTestMain();
+
+    
+
+    // print summary of test results
+    Serial.println("- - - - - - - Summary - - - - - - -");
+    Serial.print("|     ");
+    Serial.print(testFailCount);
+    Serial.println(" test(s) failed in total    |");
+    Serial.println("- - - - - - - - - - - - - - - - - -");
+    return 1;
+
+}
