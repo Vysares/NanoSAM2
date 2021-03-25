1f4c1c3864d22c2fcd9d50a41de76a7970b3eaf2
Vysares <71406782+Vysares@users.noreply.github.com>
2021-03-25 15:39:17 -0600

    Update testNS2.py

--- GSW/TestUtility/Source/testNS2.py
+++ GSW/TestUtility/Source/testNS2.py
@@ -9,8 +9,9 @@ import re
 import ctypes
 
 # to compile to a single exe with pyinstaller run the following line from the "GSW/TestUtility" directory:
-# pyinstaller.exe --onefile --icon=source/Assets/NS2_BW.ico --windowed --name TestNS2 source/testNS2.py
+#   pyinstaller.exe --onefile --icon=source/Assets/NS2_BW.ico --windowed --name TestNS2 source/testNS2.py
 # you can then safely delete the "build" folder and spec file if you want. The exe will be in the "dist" folder.
+# note that the exe will crash on startup unless the Assets and SavedFiles folders are in the same directory.
 
 # ==== setup application window ====
 ctypes.windll.shcore.SetProcessDpiAwareness(1)
